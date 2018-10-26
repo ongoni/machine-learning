6cd352cb9924a323a24c95ec483127bc82edf605
ongoni <ongoni.fg@gmail.com>
2018-10-27 03:25:46 +0400

    add double format extension

--- src/main/kotlin/utils/Extensions.kt
+++ src/main/kotlin/utils/Extensions.kt
@@ -2,7 +2,9 @@ package utils
 
 import java.util.*
 
-fun IntRange.random() = Random().nextInt((endInclusive + 1) - start) +  start
+fun IntRange.random() = Random().nextInt((endInclusive + 1) - start) + start
+
+fun Double.format(count: Int = 6) = String.format("%.${count}f", this)
 
 fun MutableMap<String, Int>.inc(key: String) {
     this[key] = this[key]!!.plus(1)
